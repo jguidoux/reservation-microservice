5fed4452b5dba210107e188d7d60e808460a0750
jguidoux <jeremie.guidoux@tocea.com>
2017-07-22 17:31:38 +0200

    reservation works

--- code/reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ code/reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -7,36 +7,22 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.dsl.support.GenericHandler;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.MessagingException;
-import org.springframework.messaging.SubscribableChannel;
-import org.springframework.stereotype.Component;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Collection;
-import java.util.Map;
 import java.util.stream.Stream;
 
 
-@EnableBinding(ConsumerChannels.class)
+//@EnableBinding(ConsumerChannels.class)
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
@@ -56,17 +42,17 @@ public class ReservationServiceApplication {
         };
     }
 
-    @Bean
-    IntegrationFlow inboundReservationFlow(ConsumerChannels channels,
-                                           ReservationRepository rr) {
-        return IntegrationFlows
-                .from(channels.input())
-                .handle((GenericHandler<String>) (reservationName, headers) -> {
-                    rr.save(new Reservation(reservationName));
-                    return null;
-                })
-                .get();
-    }
+//    @Bean
+//    IntegrationFlow inboundReservationFlow(ConsumerChannels channels,
+//                                           ReservationRepository rr) {
+//        return IntegrationFlows
+//                .from(channels.input())
+//                .handle((GenericHandler<String>) (reservationName, headers) -> {
+//                    rr.save(new Reservation(reservationName));
+//                    return null;
+//                })
+//                .get();
+//    }
 }
 /*
 @Component
@@ -78,27 +64,27 @@ class StreamListenerComponent {
     }
 }*/
 
-interface ConsumerChannels {
-    @Input
-    SubscribableChannel input();
-}
+//interface ConsumerChannels {
+//    @Input
+//    SubscribableChannel input();
+//}
 
-@RestController
-class ReservationRestController {
-
-
-    private final ReservationRepository reservationRepository;
-
-    ReservationRestController(ReservationRepository reservationRepository) {
-        this.reservationRepository = reservationRepository;
-    }
-
-    @GetMapping("/reservations")
-    Collection<Reservation> reservations() {
-        return this.reservationRepository.findAll();
-    }
-
-}
+//@RestController
+//class ReservationRestController {
+//
+//
+//    private final ReservationRepository reservationRepository;
+//
+//    ReservationRestController(ReservationRepository reservationRepository) {
+//        this.reservationRepository = reservationRepository;
+//    }
+//
+//    @GetMapping("/reservations")
+//    Collection<Reservation> reservations() {
+//        return this.reservationRepository.findAll();
+//    }
+//
+//}
 
 
 @RestController
@@ -117,25 +103,30 @@ class MessageRestController {
     }
 }
 
-@Component
-class CustomHealthIndicator implements HealthIndicator {
+//@Component
+//class CustomHealthIndicator implements HealthIndicator {
+//
+//    @Override
+//    public Health health() {
+//        return Health.status("I <3 Target!!!").build();
+//    }
+//}
 
-    @Override
-    public Health health() {
-        return Health.status("I <3 Target!!!").build();
-    }
-}
-
+@RepositoryRestResource
 interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    Collection<Reservation> findByReservationName(String rn);
+//    Collection<Reservation> findByReservationName(String rn);
 }
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Data
 @Entity
 class Reservation {
+
+    public Reservation() {
+
+    }
 
     public Reservation(String reservationName) {
         this.reservationName = reservationName;
@@ -146,4 +137,47 @@ class Reservation {
     private Long id;
 
     private String reservationName; // reservation_name
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReservationName() {
+        return reservationName;
+    }
+
+    public void setReservationName(String reservationName) {
+        this.reservationName = reservationName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Reservation that = (Reservation) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        return reservationName != null ? reservationName.equals(that.reservationName) : that.reservationName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (reservationName != null ? reservationName.hashCode() : 0);
+        return result;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id=" + id +
+                ", reservationName='" + reservationName + '\'' +
+                '}';
+    }
 }
