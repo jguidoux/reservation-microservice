adec44c5fd7d3ea4ed22c4d0a061c36e0c2fc189
jguidoux <jeremie.guidoux@tocea.com>
2017-07-22 18:09:58 +0200

    /reservations/names entry

--- code/reservation-client/src/main/java/com/example/reservationclient/ReservationClientApplication.java
+++ code/reservation-client/src/main/java/com/example/reservationclient/ReservationClientApplication.java
@@ -2,23 +2,24 @@ package com.example.reservationclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resources;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 //@EnableResourceServer
 //@IntegrationComponentScan
 //@EnableCircuitBreaker
-//@EnableFeignClients
+@EnableFeignClients
 //@EnableBinding(ProducerChannels.class)
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -50,15 +51,16 @@ public class ReservationClientApplication {
 @RequestMapping("/reservations")
 class ReservationApiAdapterRestController {
 
-//    private final ReservationReader reservationReader;
+    private final ReservationReader reservationReader;
 //    private final ReservationWriter reservationWriter;
 //
-//    ReservationApiAdapterRestController(ReservationWriter reservationWriter,
-//                                        ReservationReader reservationReader) {
-//        this.reservationReader = reservationReader;
+    ReservationApiAdapterRestController(
+            //ReservationWriter reservationWriter,
+                                        ReservationReader reservationReader) {
+        this.reservationReader = reservationReader;
 //        this.reservationWriter = reservationWriter;
-//
-//    }
+
+    }
 
     public Collection<String> fallback() {
         return new ArrayList<>();
@@ -70,24 +72,24 @@ class ReservationApiAdapterRestController {
 //    }
 //
 //    @HystrixCommand(fallbackMethod = "fallback")
-//    @GetMapping("/names")
-//    public Collection<String> names() {
-//        return reservationReader
-//                .read()
-//                .stream()
-//                .map(Reservation::getReservationName)
-//                .collect(Collectors.toList());
-//    }
+    @GetMapping("/names")
+    public Collection<String> names() {
+        return reservationReader.read()
+                .getContent()
+                .stream()
+                .map(Reservation::getReservationName)
+                .collect(Collectors.toList());
+    }
 }
 
 
-//@FeignClient("reservation-service")
-//interface ReservationReader {
-//
-//    @RequestMapping(method = RequestMethod.GET, value = "/reservations")
-//    Collection<Reservation> read();
-//
-//}
+@FeignClient("reservation-service")
+interface ReservationReader {
+
+    @GetMapping("/reservations")
+    Resources<Reservation> read();
+
+}
 
 
 class Reservation {
